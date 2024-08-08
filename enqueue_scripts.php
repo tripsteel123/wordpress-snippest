0d06efe26d4969e2b099b0c4c6d3b4b750f25fc2
Masoud <48360242+tripsteel123@users.noreply.github.com>
2024-08-08 12:06:27 +0330

    Update enqueue_scripts.php

--- enqueue_scripts.php
+++ enqueue_scripts.php
@@ -1,7 +1,7 @@
 <?php
 //for enqueue scripts in login&register pages use This:
 function themeslug_enqueue_style() {
-	 wp_enqueue_style( 'style-name', get_stylesheet_uri() );
+   wp_enqueue_style( 'style-name', get_stylesheet_uri() );
    wp_enqueue_script( 'script-name', get_template_directory_uri() . '/js/example.js', array(), '1.0.0', true );
 }
 
