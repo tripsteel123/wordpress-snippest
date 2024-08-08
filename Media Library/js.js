44e1bd53b3276befc642b04aea954d9932a9169c
Masoud <48360242+tripsteel123@users.noreply.github.com>
2024-08-08 13:18:43 +0330

    Update js.js

--- Media Library/js.js
+++ Media Library/js.js
@@ -1,1 +1,32 @@
+jQuery(document).ready(function ($) {
+    var file_frame;
+    $('#mmlp_file_button').on('click', function (event) {
+        event.preventDefault();
+
+        // اگر فریم باز است، استفاده از آن
+        if (file_frame) {
+            file_frame.open();
+            return;
+        }
+
+        // ایجاد فریم جدید
+        file_frame = wp.media({
+            title: 'Select or Upload Media',
+            button: {
+                text: 'Use this media'
+            },
+            multiple: false
+        });
+
+        // انتخاب فایل
+        file_frame.on('select', function () {
+            var attachment = file_frame.state().get('selection').first().toJSON();
+            $('#mmlp_file').val(attachment.id);
+            $('#mmlp_file_preview').html('<img src="' + attachment.url + '" style="max-width: 300px;" />');
+        });
+
+        // باز کردن فریم
+        file_frame.open();
+    });
+});
 
