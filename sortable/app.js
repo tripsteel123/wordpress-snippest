65577e1ef0f218d2167e3166847c14d743dbc415
Masoud <48360242+tripsteel123@users.noreply.github.com>
2024-08-11 11:58:55 +0330

    Update app.js

--- sortable/app.js
+++ sortable/app.js
@@ -3,9 +3,16 @@ jQuery(document).ready(function(){
         handle: '.dashicons-move',
         stop: reindex_table
     });
-    function reindex_table () {
+        function reindex_table() {
         jQuery('.playlist_table tbody tr td:nth-child(2)').each(function (index) {
-            jQuery(this).text( index + 1 );
+            jQuery(this).text(index + 1);
         });
     }
+
+    jQuery('.playlist_table .dashicons-trash').on('click', function () {
+        jQuery(this).closest('tr').addClass('deleting').slideUp(1500, function () {
+            jQuery(this).remove();
+            reindex_table();
+        });
+    });
 });
