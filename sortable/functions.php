a67b89911d1bba899a6eeb68a6c2433c88437c04
Masoud <48360242+tripsteel123@users.noreply.github.com>
2024-08-11 11:08:51 +0330

    Update functions.php

--- sortable/functions.php
+++ sortable/functions.php
@@ -1,4 +1,11 @@
 <?php
+// enqueue ui-core and ui sortable jquery
+function wpdocs_enqueue_custom_admin_style() {
+    wp_enqueue_script( 'script-name', get_template_directory_uri() . '/js/example.js', array('jquery' , 'jquery-ui-core' , 'jquery-sortable'), '1.0.0', true );
+}
+add_action( 'admin_enqueue_scripts', 'wpdocs_enqueue_custom_admin_style' );
+
+
 function add_course_meta_boxes() {
     add_meta_box(
         'course_playlist',
