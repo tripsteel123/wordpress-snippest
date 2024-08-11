ba351e4fe1e977b439ea8b53358be8f977d9030f
Masoud <48360242+tripsteel123@users.noreply.github.com>
2024-08-11 10:54:52 +0330

    Update functions.php

--- sortable/functions.php
+++ sortable/functions.php
@@ -1,1 +1,12 @@
-
+<?php
+function add_course_meta_boxes() {
+    add_meta_box(
+        'course_playlist',
+        'Course Play List',
+        'render_course_playlist_meta_box',
+        'course',
+        'normal',
+        'default'
+    );
+}
+add_action('add_meta_boxes', 'add_course_meta_boxes');
