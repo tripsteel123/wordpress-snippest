306ab6093dd2d6626dc721b36aef2490a90c113c
Masoud <48360242+tripsteel123@users.noreply.github.com>
2024-08-22 20:32:24 +0330

    Update create_table_in_activate_plugin.php

--- wpdb-database/create_table_in_activate_plugin.php
+++ wpdb-database/create_table_in_activate_plugin.php
@@ -1,3 +1,22 @@
 <?php
 
+register_activation_hook(__FILE__, 'management_users_activate');
+function management_users_activate()
+{
+    global $wpdb;
+    ## get code by SHOW CREATE TABLE wp_prousers
+    $tbl_name = $wpdb->prefix . "prousers";
+    $sql = "
+    CREATE TABLE `$tbl_name` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `full_name` varchar(255) NOT NULL,
+  `birthday` date NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`ID`)
+    ) 
+        ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
+    ";
+    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+    dbDelta( $sql );
+}
 
