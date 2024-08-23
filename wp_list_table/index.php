983f0130e157121baa83f3181009f9bf681dcddd
Masoud <48360242+tripsteel123@users.noreply.github.com>
2024-08-23 13:19:03 +0330

    Update index.php

--- wp_list_table/index.php
+++ wp_list_table/index.php
@@ -1,1 +1,8 @@
+<?php
+if ( ! class_exists( 'WP_List_Table' ) ) {
+    require_once ABSPATH . 'wp-admin/includes/class-wp-list-table.php';
+}
+class WP_LIST_TABLE_EMP extends WP_List_Table {
+    // تنظیمات اولیه (تعداد آیتم‌ها در هر صفحه)
 
+}
